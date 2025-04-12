e3b8343f504d8fde6fef42be299198560a8a0715
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2025-04-12 22:41:32 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# beta
+PS5 XPLOIT HOST auto load etaHEN 2.0b  by @m0ur0ne  
+
+24/03/2025 add new UMTX2 exploit.  thanks to idlsauce 
+
+The exploit code is largely based on the lua implementation by @shahrilnet and @n0llptr: https://github.com/shahrilnet/remote_lua_loader/blob/main/payloads/umtx.lua
+
+The rest of the setup is based on the previous works by @SpecterDev and @ChendoChap: https://github.com/PS5Dev/PS5-UMTX-Jailbreak/
+
+Thank you to them and everyone else who worked on the umtx and psfree exploit!
+
+Supports PS5 firmware 1.00-5.50
+Includes payload menu
+Uses PSFree 150b by abc
+Auto-loads @john-tornblom's ELF loader
+Includes the 9020 elf loader for compatibility with older payloads (not available in webkit-only mode)
+Webkit-only mode for sending payloads and clearing appcache
+
+
+
+
+------ UMTX2 ------
+https://ps5xploit.github.io/umtx/
+
+
+
+
+
+
+
+------ UMTX v1 ------
+https://ps5xploit.github.io/umtx1/
---
+++ payload_map.js
@@ -0,0 +1,68 @@
+// @ts-check
+
+const CUSTOM_ACTION_APPCACHE_REMOVE = "appcache-remove";
+
+/**
+ * @typedef {Object} PayloadInfo
+ * @property {string} displayTitle
+ * @property {string} description
+ * @property {string} fileName - path relative to the payloads folder
+ * @property {string} author
+ * @property {string} projectSource
+ * @property {string} binarySource - should be direct download link to the included version, so that you can verify the hashes
+ * @property {string} version
+ * @property {string[]?} [supportedFirmwares] - optional, these are interpreted as prefixes, so "" would match all, and "4." would match 4.xx, if not set, the payload is assumed to be compatible with all firmwares
+ * @property {number?} [toPort] - optional, if the payload should be sent to "127.0.0.1:<port>" instead of loading directly, if specified it'll show up in webkit-only mode too
+ * @property {string?} [customAction]
+ */
+
+/**
+ * @type {PayloadInfo[]}
+*/
+const payload_map = [
+    // { // auto-loaded
+    //     displayTitle: "PS5 Payload ELF Loader",
+    //     description: "Uses port 9021. Persistent network elf loader",
+    //     fileName: "elfldr.elf",
+    //     author: "john-tornblom",
+    //     projectSource: "https://github.com/ps5-payload-dev/elfldr",
+    //     binarySource: "https://github.com/ps5-payload-dev/elfldr/releases/download/v0.19/Payload.zip",
+    //     version: "0.19",
+    //     supportedFirmwares: ["1.", "2.", "3.", "4.", "5."]
+    // },
+    // etaHEN is added twice so that on 1.xx-2.xx you can load it in webkit only mode too
+    // but on 3.xx-4.xx it only shows in kernel exploit mode since it needs the 9020 elf loader for kstuff
+    {
+        displayTitle: "etaHEN",
+        description: "AIO HEN",
+        fileName: "etaHEN.bin",
+        author: "LightningMods, Buzzer, sleirsgoevy, ChendoChap, astrelsky, illusion, CTN, SiSTR0, Nomadic",
+        projectSource: "https://github.com/LightningMods/etaHEN",
+        binarySource: "https://github.com/LightningMods/etaHEN/releases/download/1.9b/etaHEN.bin",
+        version: "2.0b",
+        supportedFirmwares: ["3.", "4."]
+    },
+     {
+        displayTitle: "kstuff-550",
+        description: "kstuff-550",
+        fileName: "kstuff-550.elf",
+        author: "kstuff-550",
+        projectSource: "kstuff-550",
+        binarySource: "kstuff-550",
+        version: "5.50",
+        supportedFirmwares: ["5", "550"]
+    },
+   
+   // {
+   //     // https://github.com/Storm21CH/PS5_Browser_appCache_remove
+   //     displayTitle: "Browser appcache remover",
+    //    description: "Deletes for only the current user in webkit-only mode",
+    //    fileName: "",
+    //    author: "Storm21CH, idlesauce",
+    //    projectSource: "",
+     //   binarySource: "",
+    //    version: "1.0",
+    //    customAction: CUSTOM_ACTION_APPCACHE_REMOVE
+  //  }
+
+];
